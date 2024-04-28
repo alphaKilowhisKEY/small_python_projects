20d41bc580d0f4b42dd0d4d2240f536bc5d413c1
kristina <kristina.aleksandrova@student.uibk.ac.at>
2024-04-28 22:12:51 +0200

    gui quiz

--- gui_quiz_app/main.py
+++ gui_quiz_app/main.py
@@ -1,3 +1,27 @@
+
+"""
+Quizzier - A simple quiz application using tkinter
+
+This Python script creates a simple quiz application using the tkinter library. It displays questions one by one and allows users to answer by clicking on True or False buttons. After answering, it provides immediate feedback by changing the background color of the question area to green (for correct) or red (for incorrect). At the end of the quiz, it displays the user's final score.
+
+Libraries Used:
+- tkinter: for building the graphical user interface
+- quiz_brain: for managing the quiz questions and answers
+
+Global Variables:
+- THEME_COLOR: Theme color used for the background
+- FONT: Font configuration for text
+
+Classes:
+- QuizInterface: Class representing the GUI of the quiz application
+- QuizBrain: manages the list of questions, keeps track of the current question number and score, 
+   provides methods to retrieve the next question, check the user's answer, and determine if there are any remaining questions.
+- Qestion: represents a single question in a quiz.
+
+Usage:
+- Run the script to start the quiz application: $python3 main.py
+"""
+
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
